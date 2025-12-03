d1f0a2e4309354248e3751bf48994e0c02b259d3
Cássio Ávila <cassioavila@protonmail.com>
2025-12-03 17:25:32 -0300

    cleanup

--- cmd/codewoot/main.go
+++ cmd/codewoot/main.go
@@ -1,34 +1,11 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
 	"log"
 
 	"github.com/sdrvirtual/codewoot/internal/config"
 	"github.com/sdrvirtual/codewoot/internal/server"
-	"github.com/sdrvirtual/codewoot/internal/services"
 )
-
-func Cu() {
-	cfg, _ := config.Load()
-	service := services.NewChatwootService(cfg)
-
-	contact := services.ContactInfo{
-		Name: "Cássio Ávila",
-		Phone: "+5534988781744",
-	}
-
-	ctx := context.TODO()
-
-	ctt, err := service.SetupContact(ctx, &contact)
-	if err != nil {
-		log.Fatal(err)
-	}
-	s, _ := json.MarshalIndent(ctt, "", "\t")
-	fmt.Println(string(s))
-}
 
 func main() {
 	cfg, _ := config.Load()
